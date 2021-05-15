4a81fc23fac31a51b119e8d0025a77524360a0df
HCL <abhinavjha2968@gmail.com>
2021-05-15 21:00:50 +0530

    Get recipe data from db in json format.

--- chef.py
+++ chef.py
@@ -4,3 +4,13 @@ import datetime
 
 connection = sqlite3.connect("recipes.db")
 cursor = connection.cursor()
+
+rows = cursor.execute("SELECT * FROM Recipes")
+object_list = []
+recipe_list = []
+
+for row in rows:
+    object_list.append({"Recipe_ID": row[0], "Recipe_Name": row[1],
+                        "Recipe_Desc": row[2]})
+
+recipe_json = json.loads(json.dumps({"Recipes": object_list}, indent=1))
