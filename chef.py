15ec4054ccb28e7dc360bc067d7e4b47217df1ba
HCL <abhinavjha2968@gmail.com>
2021-05-16 19:35:10 +0530

    Get order data from db in json format

--- chef.py
+++ chef.py
@@ -17,3 +17,12 @@ recipe_json = json.loads(json.dumps({"Recipes": object_list}, indent=1))
 
 for recipe in recipe_json["Recipes"]:
     recipe_list.append(recipe["Recipe_Name"])
+
+order_rows = cursor.execute("SELECT * FROM MasterChef")
+object_list2 = []
+
+for row in order_rows:
+    object_list2.append({"Order_ID": row[0], "Order": row[1],
+                        "Recipe_Desc": row[2]})
+
+order_json = json.loads(json.dumps({"Orders": object_list2}, indent=1))
