9efe86a8e530e48c62e15b21fd151118caa860f7
HCL <abhinavjha2968@gmail.com>
2021-05-16 20:01:31 +0530

    Added if condition for printing the recipe. Some errors fixed.

--- chef.py
+++ chef.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 import datetime
+import random
 
 connection = sqlite3.connect("recipes.db")
 cursor = connection.cursor()
@@ -22,7 +23,17 @@ order_rows = cursor.execute("SELECT * FROM MasterChef")
 object_list2 = []
 
 for row in order_rows:
-    object_list2.append({"Order_ID": row[0], "Order": row[1],
-                        "Recipe_Desc": row[2]})
+    object_list2.append({"Order_ID": row[0], "Customer_Name": row[1],
+                        "Order_Recipe": row[2], "Order_Date": row[3]})
 
 order_json = json.loads(json.dumps({"Orders": object_list2}, indent=1))
+
+username = input("Username: ")
+
+if len(order_json["Orders"]) == 0:
+    date = datetime.datetime.now().date()
+    recipe = random.choice(recipe_list)
+    cursor.execute("INSERT INTO MasterChef VALUES(?, ?, ?, ?)",
+                   (0, "Abhinav", recipe, date))
+    connection.commit()
+    print(recipe)
